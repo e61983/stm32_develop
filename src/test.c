441d77b325430f747f3a379d44f06f2f05f7d6f4
Yuan <e61983@gmail.com>
2016-09-04 17:35:36 +0800

    test semihosting

--- src/test.c
+++ src/test.c
@@ -1,5 +1,14 @@
-#include <stdio.h>
+#include <string.h>
+#include "semi.h"
+
 int main(void){
-    printf("HELLO\n");
+    const char *hello = "HELLO\n";
+
+    write(1,hello,strlen(hello));
+
+    int fp = open("log.txt", 4);
+    write(fp,hello,strlen(hello));
+    close(fp);
+
     while(1);
 }
